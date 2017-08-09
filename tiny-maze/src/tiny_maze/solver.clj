d23e85e9560969c5f2029446fea49a6e6a0d5396
Steven Thonus <steven@ln2.nl>
2017-08-09 21:28:59 +0200

    solved puzzle 1

--- tiny-maze/src/tiny_maze/solver.clj
+++ tiny-maze/src/tiny_maze/solver.clj
@@ -1,3 +1,64 @@
 (ns tiny-maze.solver)
 
-(defn solve-maze [maze])
+(def maze1 [[:S 0 1]
+            [1  0 1]
+            [1  0 :E]])
+
+(defn update-in-maze [poses maze]
+  (let [pos (first poses)]
+    (if pos
+      (recur (rest poses)
+             (assoc-in maze [(first pos) (last pos)] :x))
+      maze)))
+
+(defn next-pos [x y]
+  [
+   [(dec x) y]
+   [x (dec y)]
+   [(inc x) y]
+   [x (inc y)]
+   ]
+  )
+
+(defn maze-val-for-pos [maze x y]
+  (try (nth (nth maze x) y)
+       (catch Exception e
+         :X
+         ))
+  )
+
+(defn valid-pos [poses maze]
+  (filter (fn [pos]
+            (let [x (first pos)
+                  y (second pos)
+                  pos-val (maze-val-for-pos maze x y)]
+              (or (= 0 pos-val) (= :S pos-val) (= :E pos-val)))
+            )
+          poses)
+  )
+
+(defn in?
+  "true if coll contains elm"
+  [coll elm]
+  (some #(= elm %) coll))
+
+(defn rej-pos [poses path]
+  (filter (fn [pos]
+            (not (in? path pos)) )
+          poses))
+
+(defn keep-going [x y path maze]
+  (let [next-poses (next-pos x y)
+        next-poses (valid-pos next-poses maze)              ;; does not have options => go back
+        next-poses (rej-pos next-poses path)
+        next-pos (first next-poses)
+        next-path (conj path next-pos)]
+    (if next-pos
+      (recur (first next-pos) (last next-pos) next-path maze)
+      (do
+        (println (str path))
+        path))))
+
+(defn solve-maze [maze]
+  (let [walked-path (keep-going 0 0 [[0 0]] maze)]
+    (update-in-maze walked-path maze)))
